2609dd37440cdac7be7c9f61104d8ebdd3315bf2
Tarun Padath <tarunpadath@gmail.com>
2019-09-04 09:46:12 -0700

    update Pyramid Class

---
+++ pyramid.js
@@ -0,0 +1,14 @@
+class Pyramid {
+  constructor (currentPlayer, blockArray){
+    this.currentPlayer = currentPlayer;
+    this.blockArray = blockArray;
+    this.pointsArray = [3,1,2,3,4,2,3,1,2,3,2,3,1,4];
+    this.domElements = {
+      pyramidContainer: null,
+      pyramidBlocks: null,
+    }
+  }
+  render(){
+
+  }
+}
