49d0d99c01d6c36b11e3bc62b2fe6038d21cc577
Noel Carino <leononirac1992@gmail.com>
2019-09-04 10:49:22 -0700

    added new files

---
+++ IMHOTEP/imhotep.js
@@ -0,0 +1,18 @@
+class Imhotep {
+    constructor (displayDom) {
+        this.dom = {
+            container = $(displayDom)
+        }
+        
+        this.dom.container.append( boardElements );
+        // var boardElements = this.render();
+
+    }
+
+    // render the game boards here
+    render(){
+        this.dom.container = $("<div>", {
+            class: 'gameArea',
+        })
+    }
+}
---
+++ IMHOTEP/main.js
@@ -0,0 +1,9 @@
+var game;
+
+$(document).ready( startApp );
+
+function startApp(){
+    debugger;
+    game = new Imhotep('body')
+    console.log("test");
+}
