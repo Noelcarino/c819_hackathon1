52a5a0805d60f2894b3202367e887abee1e8fc9d
Tarun Padath <tarunpadath@gmail.com>
2019-09-04 18:04:56 -0700

    Finished pyramid logic

--- IMHOTEP/main.js
+++ IMHOTEP/main.js
@@ -4,12 +4,17 @@ $(document).ready( startApp );
 
 function startApp(){
     game = new Imhotep('body');
+    var pyramid = new Pyramid(1, ['black', 'white', 'black', 'black']);
+    boat = new Boat();
     $("#button").click(function(){
         game.startGame();
-
+        pyramid.render();
+        pyramid.addPyramidBlocks();
+        pyramid.doScore();
+        var blackScore = pyramid.getBlackScore();
+        var whiteScore = pyramid.getWhiteScore();
+        console.log(blackScore);
+        console.log(whiteScore);
+        boat.render();
     })
-    
-    boat = new Boat();
-    boat.render();
 }
-
--- IMHOTEP/pyramid.js
+++ IMHOTEP/pyramid.js
@@ -2,11 +2,43 @@ class Pyramid {
   constructor(currentPlayer, blockArray) {
     this.currentPlayer = currentPlayer;
     this.blockArray = blockArray;
+    this.blackScore = null;
+    this.whiteScore = null;
+    this.allBlocksForRoundArray = [];
     this.pointsArray = [3, 1, 2, 3, 4, 2, 3, 1, 2, 3, 2, 3, 1, 4];
     this.domElements = {
-      pyramidContainer: null,
-      pyramidBlocks: null,
+      pyramidContainer: null
     }
+  }
+  doScore (){
+    for (var integerI = 0;integerI < this.allBlocksForRoundArray.length; integerI++){
+      if (this.allBlocksForRoundArray[integerI] === 'black') {
+        this.blackScore += this.pointsArray[integerI];
+      }
+      else {
+        this.whiteScore += this.pointsArray[integerI];
+      }
+
+    }
+  }
+  getBlackScore () {
+    return this.blackScore;
+  }
+
+  getWhiteScore() {
+    return this.whiteScore;
+  }
+
+  addPyramidBlocks (){
+    for (var integerI = 0; integerI< this.blockArray.length; integerI++ ){
+      this.allBlocksForRoundArray.push(this.blockArray[integerI]);
+      var pyramidBlocks = $("<div>").addClass('pyramid-blocks');
+      pyramidBlocks.css({
+        'background-color':this.blockArray[integerI]
+      });
+      this.domElements.pyramidContainer.append(pyramidBlocks);
+    }
+
   }
   render() {
     this.domElements.pyramidContainer = $("<div>", {
@@ -15,11 +47,11 @@ class Pyramid {
         text: "Pyramid",
       }
     });
-    var boardContainer = $(".container.board");
+    var boardContainer = $(".pyramid-container");
     boardContainer.append(this.domElements.pyramidContainer);
-    this.domElements.pyramidBlocks = $("<div>", {
-      class: 'pyramid-blocks',
-    })
-    this.domElements.pyramidContainer.append(this.domElements.pyramidBlocks);
+    // this.domElements.pyramidBlocks = $("<div>", {
+    //   class: 'pyramid-blocks',
+    // })
+    // this.domElements.pyramidContainer.append(this.domElements.pyramidBlocks);
   }
 }
